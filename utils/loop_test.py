c1094f0a0991be3fc199cacf4e54d7ba3f364162
zzq89757 <1604653835@qq.com>
2024-09-29 16:51:37 +0800

    loop test start

---
+++ utils/loop_test.py
@@ -0,0 +1,46 @@
+from collections import defaultdict
+
+def loop_detective(duplex_str: str) -> None:
+    seq1, seq2 = duplex_str.split()
+
+
+def re_test(md_tag: str) -> list[str]:
+    """
+    md标签反应参考上的碱基情况
+    当出现^时 read相较于参考有缺失 直接增加^后的碱基数 保证位置不偏移
+    若read相较于参考有插入 需看CigarI标签 不添加碱基数
+    """
+    import re
+
+    # md_tag_li = re.split(r'([ACTG^])', md_tag)
+    # md_tag_li = [x for x in  re.split(r'(\d+)', md_tag) if x]
+    md_tag_li = re.split(r"(\d+)", md_tag)
+    md_tag_li = list(filter(None, md_tag_li))
+    print(md_tag_li)
+
+
+def cigar_test(reference_start: int, cigar_str: str) -> defaultdict:
+    import re
+    insertion_dict = defaultdict()
+    # extract to a indival function:has_insertion
+    if cigar_str.find("I") == -1:
+        return insertion_dict
+    # only preverse cigar_str until last I
+    cigar_li = re.split(r"(\d+)", cigar_str)
+    cigar_li = list(filter(None, cigar_li))
+    current_pos = reference_start
+    for idx in range(0, len(cigar_li), 2):
+        region_length = cigar_li[idx]
+        tag = cigar_li[idx + 1]
+        # I not add pos, D add
+        if tag != "I":
+            current_pos += region_length
+        
+        if tag == "I":
+            insertion_dict[current_pos] = str(region_length)
+        
+
+
+if __name__ == "__main__":
+    # re_test("506^CA9C17A0C0A18^G9^C11")
+    cigar_test(100, "89M1I11M38S")
