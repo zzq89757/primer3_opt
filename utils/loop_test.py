66503d7efc7e2f704020704f01c0bf0bf44dc7a0
zzq89757 <1604653835@qq.com>
2024-10-08 15:16:24 +0800

    region found finished

--- utils/loop_test.py
+++ utils/loop_test.py
@@ -3,19 +3,61 @@ from collections import defaultdict, deque
 
 def loop_detective(duplex_str: str) -> None:
     """
-    检测分子杂交(duplex)中的loop结构(internal loop和bulge loop)
+    检测分子杂交(duplex)中的loop结构(internal loop 和 bulge loop)
 
     """
     gap_index_li = deque()
+    match_asc_li = [149, 138]  # ord sum is  AT: 65 + 84 = 149   CG: 67 + 71 = 138
+    gap_asc_li = [90, 129, 116, 112, 110]
+    bulge_start_flag = 0
+    intloop_start_flag = 0
     seq1, seq2 = duplex_str.split("\n")
     # record pairs and gaps,then recogonize bulge loop and internal loop
     for idx in range(len(seq1)):
         # gap found, record index
-        if seq1[idx] == '-' or seq2[idx] == '-':
+        if seq1[idx] == "-" or seq2[idx] == "-":
             gap_index_li.append(idx)
-        
-    # cycle 2, calc 
-            
+    region_li = deque()  # record region start index and end index
+    region_type = 0  # 0 for bulge,1 for int loop
+    for idx in range(len(seq1)):
+        ord_sum = ord(seq1[idx]) + ord(seq2[idx])
+        if ord_sum not in match_asc_li:  # gap or mismatch start
+            if ord_sum in gap_asc_li:  # gap
+                # record region start
+                region_li.append(idx)
+
+    ord_sum_li = [ord(x) + ord(y) for x, y in zip(seq1, seq2)]
+    region_li = deque()  # region start and end index
+    region_type_flag = 0  # 0 for bulge,1 for int loop
+    region_type_li = deque()  # storage region_type_flag
+    flag = 0  # switch to record region start and end index
+    print(ord_sum_li)
+    for i, v in enumerate(ord_sum_li):
+        if v not in match_asc_li and not flag:
+            region_li.append(i)
+            flag = 1
+        if v not in gap_asc_li and v not in match_asc_li:
+            region_type_flag = 1
+        if v in match_asc_li and flag:
+            region_li.append(i - 1)
+            region_type_li.append(region_type_flag)
+            flag = 0
+            region_type_flag = 0
+    print(region_li)
+    print(region_type_li)
+
+
+def sum_test() -> None:
+    asc_li = [65, 67, 71, 84, 45]
+    base_li = ["A", "C", "G", "T", "-"]
+    for i in range(len(asc_li)):
+        for j in range(i, len(asc_li)):
+            print(f"{base_li[i]}>{base_li[j]}", end="\t")
+            print(asc_li[i] + asc_li[j])
+
+
+# sum_test()
+# exit()
 
 
 def md_tag_detective(md_tag: str) -> list[str]:
@@ -63,4 +105,4 @@ def snp_check(reference_start: int, cigar_str: str, md_tag: str | None) -> None:
 if __name__ == "__main__":
     # re_test("506^CA9C17A0C0A18^G9^C11")
     # cigar_test(100, "89M1I11M38S22M5I30M")
-    loop_detective("GCTAGCATCGTAGCT\nCGTAGCTGATGCGTA")
+    loop_detective("GCTAGCATCGTA--GCT\nCGTAGCTGATGCTTGTA")
