b0446e97d578c842139e8d204476623d593cbe37
zzq89757 <1604653835@qq.com>
2024-10-11 06:43:57 +0000

    int11 AT finished

--- utils/NN.py
+++ utils/NN.py
@@ -281,9 +281,62 @@ def int_loop_energy(segment1: str, segment2: str) -> list:
 
 
 def calc_Tm_by_NN(duplex_str: str, loop_region_dict: defaultdict) -> float:
-    dS = 0
-    dG = 0
-    dH = 0
+    # int loop energy matrix
+    int_loop_energy_dict = defaultdict(deque)
+    int_loop_energy_dict['11dg'] = deque([
+        # A
+        # T 
+        [
+            # A A
+            # T T
+            [
+                1.3, 2.2, 0.9, 1.0, # AA AC AG AT
+                1.7, 2.4, 1.0, 1.4, # CA CC CG CT
+                1.7, 2.4, 1.0, 1.4, # GA GC GG GT
+                1.7, 2.4, 1.0, 1.4, # TA TC TG TT           
+            ],
+            # A C
+            # T G
+            [
+                0.8, 1.4, -0.4, 1.0, # AA AC AG AT
+                1.6, 2.1, 1.0, 1.6, # CA CC CG CT
+                -0.2, 1.0, -1.2, 2.0, # GA GC GG GT
+                1.0, 1.4, 2.0, 1.1, # TA TC TG TT    
+            ],
+            # A G
+            # T C
+            [
+                1.0, 1.7, 0.3, 1.0, # AA AC AG AT
+                1.5, 2.0, 1.0, 1.0, # CA CC CG CT
+                0.1, 1.0, -0.3, 2.0, # GA GC GG GT
+                1.0, 1.4, 2.0, 0.6, # TA TC TG TT 
+            ],
+            # A T
+            # T A
+            [
+                1.2, 1.7, 0.2, 1.0, # AA AC AG AT
+                1.7, 2.7, 1.0, 1.4, # CA CC CG CT
+                0.2, 1.0, -0.3, 2.0, # GA GC GG GT
+                1.0, 1.4, 2.0, 1.4, # TA TC TG TT 
+            ]
+            
+        ],
+        # C
+        # G
+        [
+            # C A
+            # G T
+            [
+                
+            ]
+        ]
+        
+    ])
+    
+    # init variable
+    dH = dS = 0
+    
+    
 
     seq1, seq2 = duplex_str.split("\n")
     region_pos_li = loop_region_dict["region_pos"]
